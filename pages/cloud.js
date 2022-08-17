3f5bbaf50b294e3efbbdd65843a712c380720b5f
Antoine <bulletforaddicts@icloud.com>
2022-08-17 21:48:14 +0000

    SNIF

--- pages/cloud.js
+++ pages/cloud.js
@@ -33,14 +33,7 @@ export default function Home() {
 	<div>
 	  <main>
 		<div>
-		  <iframe src="https://cloud.stylo.social/index.php/s/RCrzWD6DdKY63kC" frameBorder="0"
-		  position: fixed;
-		  background: #000;
-		  border: none;
-		  top: 0; right: 0;
-		  bottom: 0; left: 0;
-		  width: 100%;
-		  height: 100%;></iframe>  
+		  <iframe src="https://cloud.stylo.social/index.php/s/RCrzWD6DdKY63kC" frameBorder="0"></iframe>  
 		</div>
 		
 	
